dc868ead1f80292ced547f5f301137e7b3b4f9df
pacepi <p_ace@126.com>
2018-01-08 17:49:48 +0800

    add objdump-function.sh
    "objdump -D vmlinux" is too slow.
    Ex, objdump-function.sh vmlinux do_fork is faster.

---
+++ objdump-function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+vmlinux=$1 
+symbol=$2 
+ 
+if [ -z "$vmlinux" ]; then 
+	echo "usage : $0 vmlinux symbol"
+    exit 
+fi 
+ 
+startaddress=$(nm -n $vmlinux | grep "\w\s$symbol" | awk '{print "0x"$1;exit}') 
+endaddress=$(nm -n $vmlinux | grep -A1 "\w\s$symbol" | awk '{getline; print "0x"$1;exit}') 
+  
+if [ -z "$symbol" ]; then 
+	echo "dump all symbol"
+    objdump -d $vmlinux 
+else 
+    echo "start-address: $startaddress, end-address: $endaddress" 
+    objdump -d $vmlinux --start-address=$startaddress --stop-address=$endaddress
+fi
