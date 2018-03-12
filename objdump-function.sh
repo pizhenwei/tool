dd36852e3b616a44eda1b4ab63b0f59599344d72
zhenwei.pi <zhenwei.pi@youruncloud.com>
2018-03-12 09:35:53 +0800

    use grep -w for objdump-function.sh

--- objdump-function.sh
+++ objdump-function.sh
@@ -8,8 +8,8 @@ if [ -z "$vmlinux" ]; then
     exit 
 fi 
  
-startaddress=$(nm -n $vmlinux | grep "\w\s$symbol" | awk '{print "0x"$1;exit}') 
-endaddress=$(nm -n $vmlinux | grep -A1 "\w\s$symbol" | awk '{getline; print "0x"$1;exit}') 
+startaddress=$(nm -n $vmlinux | grep -w "$symbol" | awk '{print "0x"$1;exit}') 
+endaddress=$(nm -n $vmlinux | grep -w -A1 "$symbol" | awk '{getline; print "0x"$1;exit}') 
   
 if [ -z "$symbol" ]; then 
 	echo "dump all symbol"
